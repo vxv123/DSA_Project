76bb1e18b39186373e462d39209e1d6608015050
Unknown <wesleyaegaz@gmail.com>
2018-04-18 14:08:47 -0400

    Added custom objects for Party and Table

---
+++ Party.java
@@ -0,0 +1,13 @@
+public class Party{
+   private String name;
+   private int size;
+   private boolean hasPet;
+   //aggregates name, size, and section preference
+   
+   public Party(String n, int s, boolean p){
+      name = n;
+      size = s;
+      hasPet = p;
+   }
+
+}
---
+++ Table.java
@@ -0,0 +1,16 @@
+public class Table{
+//aggregates information related to its unique name, maximum capacity, section, and the Party
+   private final String name;
+   private final int capacity;
+   private Party guests;
+   
+   public Table(String unique_name, int n){
+      name = unique_name;
+      capacity = n; //Capacity can never change for a given table- tables can be removed or added instead
+      guests = null;
+   }
+   
+   public void seat(Party p){
+      guests = p;
+   }
+}
